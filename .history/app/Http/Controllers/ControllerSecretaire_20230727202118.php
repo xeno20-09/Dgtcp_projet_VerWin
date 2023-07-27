c90ca69b73927af95952fe2d9ef8111215cccabf
xeno20-09 <allegressecakpo93@gmail.com>
2023-07-27 20:28:53 +0200

    controller/model/migration/web

---
+++ .history/app/Http/Controllers/ControllerSecretaire_20230727202118.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ControllerSecretaire extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+ 
+}
---
+++ .history/app/Models/demandes_20230727202615.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class demandes extends Model
+{
+    use HasFactory;
+}
