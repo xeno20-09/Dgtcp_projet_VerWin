792dae28eee3fe2c54c4e1de05f0b47db37308d2
xeno20-09 <allegressecakpo93@gmail.com>
2023-07-27 20:51:12 +0200

    web/migration

--- .history/database/migrations/2014_10_12_000000_create_users_table_20230630081813.php
+++ .history/database/migrations/2014_10_12_000000_create_users_table_20230630081813.php
@@ -15,12 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('poste')->default('Client');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->timestamp('last_login_at')->nullable();
         });
     }
 
