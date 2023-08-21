da0563ad86d3eddd0725da79c4cdc7baaa733c02
xeno20-09 <allegressecakpo93@gmail.com>
2023-08-21 17:14:46 +0200

    addfonction

---
+++ .history/resources/views/chef_bureau/liste_demande.blade_20230821165904.php
@@ -0,0 +1,81 @@
+@extends('layout.chef_bureau.header')
+@section('content')
+    <div class="container">
+        <h1 style="text-align: center;">
+            @foreach ($user as $item)
+                <a class="nav-link" href="#"> Mr/Mrs {{ $item->name }} </a>
+            @endforeach
+        </h1>
+        <h1>Liste des demandes </h1>
+
+        <table class="table ">
+            <thead>
+                <tr>
+
+                    <th scope="col">N°Dossier:</th>
+                    <th scope="col">Nom du client:</th>
+                    <th scope="col">Prenom du client:</th>
+                    <th scope="col">Date:</th>
+                    <th scope="col">Nom Secretaire:</th>
+                    <th scope="col">Nom Beneficiere:</th>
+                    <th scope="col">Prenom Beneficiere:</th>
+                    <th scope="col">Nom Verificateur:</th>
+                    <th scope="col">Date decision :</th>
+                    <th scope="col">Nom Chef Division:</th>
+                    <th scope="col">Montant:</th>
+                    <th scope="col">Devise:</th>
+                    <th scope="col">Contre Montant en FCFA:</th>
+                    <th scope="col">Status:</th>
+
+                </tr>
+            </thead>
+            <tbody>
+
+                @foreach ($demande as $item)
+                    <tr class="table-success">
+
+                        <td> {{ $item->numero_doss }}</td>
+                        <td> {{ $item->nom_client }}</td>
+                        <td> {{ $item->prenom_client }}</td>
+                        <td> {{ $item->date }}</td>
+                        @foreach ($jointure as $itemc)
+                            <td> {{ $itemc->name }}</td>
+                        @break
+                    @endforeach
+
+
+                    <td>{{ $item->nom_benefi }}</td>
+                    <td>{{ $item->prenom_benefi }}</td>
+
+                    @foreach ($jointure1 as $itemd)
+                        <td> {{ $itemd->name }}</td>
+                    @break
+                @endforeach
+                <td>{{ $item->date_decision }}</td>
+                @foreach ($jointure2 as $iteme)
+                    <td> {{ $iteme->name }}</td>
+                @break
+            @endforeach
+            <td> {{ $item['montant'] }}</td>
+            <td>{{ $item['devise'] }}</td>
+            <td>{{ $item['montant_con'] }}</td>
+            <td>{{ $item->status_dmd }}</td>
+            <td>
+                <a style="width: auto; height:fit-content;"
+                    href="{{ url('detailles_demand', ['id' => $item->id]) }}" class="btn btn-primary">Voir</a>
+            </td>
+
+
+
+
+        </tr>
+    @endforeach
+</tbody>
+</table>
+
+</div>
+
+
+<br>
+<br>
+@endsection
