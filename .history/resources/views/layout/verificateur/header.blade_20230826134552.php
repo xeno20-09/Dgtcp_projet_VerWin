1d59d0090204bccfcfc620c7f438cc8fdad59abc
xeno20-09 <allegressecakpo93@gmail.com>
2023-08-26 20:39:20 +0200

    addfunction

---
+++ .history/resources/views/layout/verificateur/header.blade_20230826134552.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <title></title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+            <div class="container-fluid">
+                <img src="{{ asset('images/whiteOfficialLogo.png') }}" alt="Mon Image">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01"
+                    aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarColor01">
+                    <ul class="navbar-nav me-auto">
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('HVerificateur') }}">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('ListeDemandesN') }}">Saisir une demande</a>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                Liste
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('get_list_piece_verificateur') }}">
+                                    {{ __('Liste des pieces') }}
+                                </a>
+                                <a class="dropdown-item"
+                                    href="{{ action([\App\Http\Controllers\ControllerVerificateur::class, 'get_list_ask']) }}">
+                                    {{ __('Liste des demandes') }}
+                                </a>
+                            </div>
+                        </li>
+                        <!--   <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li> -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"> </a>
+                        </li>
+
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                    <form class="d-flex flex-row gap-3" style="width: 32%;position: relative;left: 120px;top: 72px;"
+                        action="{{ route('get_search_ask_verificateur', $item->id) }}" method="GET">
+                        <input class="form-control" type="search" name="query"
+                            value="{{ request()->input('query') }}" placeholder="Recherche" aria-label="Recherche">
+                        <button class="btn btn-outline" type="submit"><i class="fas fa-search"
+                                style="font-size:10px;"></i></button>
+                    </form>
+                </div>
+                <li style="list-style-type: none;" class="nav-item">
+                    <a href=""><i class="fas fa-bell" style="font-size:48px;"></i></a>
+                    <span class="badge rounded-pill badge-notification bg-danger"
+                        style="position: relative;bottom: 24px;right: 24px;">{{ $dmd_n_lu }}</span>
+                </li>
+
+                <img src="{{ asset('images/logo_DGTCP_2_blanc.png') }}" alt="Mon Image">
+            </div>
+
+        </nav>
+    </header>
+    <main class="py-4">
+
+
+        @yield('content')
+    </main>
+
+    @extends('layout.verificateur.footer')
+</body>
+
+</html>
---
+++ .history/routes/web_20230826140845.php
@@ -0,0 +1,121 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Auth::routes();
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/login', [App\Http\Controllers\Controller::class, 'loginUser'])->name('login');
+Route::get('/register', [App\Http\Controllers\Controller::class, 'registerUser'])->name('register');
+
+/**************************************** Debut route secretaire ********************************/
+Route::get('/HSecretaire', [App\Http\Controllers\ControllerSecretaire::class, 'home']);
+Route::get('/Formulaire', [App\Http\Controllers\ControllerSecretaire::class, 'get_form_ask'])->name('get_form_ask');
+Route::post('/FormulaireSave{id_user}', [App\Http\Controllers\ControllerSecretaire::class, 'store_form_ask'])->name('store_form_ask');
+Route::get('/ListeDemandes', [App\Http\Controllers\ControllerSecretaire::class, 'get_list_ask'])->name('get_list_ask');
+Route::get('/ListeDetaille{id_ask}', [App\Http\Controllers\ControllerSecretaire::class, 'get_list_details_ask'])->name('get_list_details_ask');
+Route::get('/RechecherDemande{id_user}',  [App\Http\Controllers\Controller::class, 'get_search_ask'])->name('get_search_ask');
+Route::get('/MiseAJourDemande{id_user}', [App\Http\Controllers\ControllerSecretaire::class, 'get_update_form_ask'])->name('get_update_form_ask');
+Route::post('/MiseAJourDemandeSave{id_user}', [App\Http\Controllers\ControllerSecretaire::class, 'store_update_form_ask'])->name('store_update_form_ask');
+Route::get('/get_search_ask_suite', [App\Http\Controllers\ControllerSecretaire::class, 'get_search_ask_suite'])->name('get_search_ask_suite');
+Route::post('/RechecherLaDemandeSuite{id}',  [App\Http\Controllers\ControllerSecretaire::class, 'store_search_ask_suite'])->name('store_search_ask_suite');
+Route::post('/FormulaireSuiteSave', [App\Http\Controllers\ControllerSecretaire::class, 'store_form_ask_suite'])->name('store_form_ask_suite');
+
+
+/*************************************** Fin route secretaire ***********************************/
+
+
+/**************************************** Debut route verificateur ********************************/
+Route::get('/HVerificateur', [App\Http\Controllers\ControllerVerificateur::class, 'home']);
+Route::get('/ListeDemandesN_verificateur', [App\Http\Controllers\ControllerVerificateur::class, 'get_list_ask_n_verificateur']);
+Route::get('/Formulaire_verificateur/{id_user}', [App\Http\Controllers\ControllerVerificateur::class, 'get_form_ask'])->name('get_form_ask_verificateur');
+Route::post('/FormulaireSave_verificateur{id_user}', [App\Http\Controllers\ControllerVerificateur::class, 'store_form_ask'])->name('store_form_ask_verificateur');
+Route::get('/ListePiece', [App\Http\Controllers\ControllerVerificateur::class, 'get_list_piece'])->name('get_list_piece_verificateur');
+Route::get('/ListeDemandes_verificateur', [App\Http\Controllers\ControllerVerificateur::class, 'get_list_ask'])->name('get_list_ask_verificateur');
+
+Route::get('/ListeDetaille{id_ask}', [App\Http\Controllers\ControllerVerificateur::class, 'get_list_details_ask'])->name('get_list_details_ask_verificateur');
+Route::get('/RechecherDemande_verificateur{id}',  [App\Http\Controllers\Controller::class, 'get_search_ask'])->name('get_search_ask_verificateur');
+Route::get('/MiseAJourDemande{id_user}', [App\Http\Controllers\ControllerVerificateur::class, 'get_update_form_ask'])->name('get_update_form_ask_verificateur');
+Route::post('/MiseAJourDemandeSave{id_user}', [App\Http\Controllers\ControllerVerificateur::class, 'store_update_form_ask'])->name('store_update_form_ask_verificateur');
+Route::post('/PiecesSave{id_user}', [App\Http\Controllers\ControllerVerificateur::class, 'store_form_piece'])->name('store_form_piece_verificateur');
+/*************************************** Fin route verificateur ***********************************/
+
+
+/**************************************** Debut route Chef division ********************************/
+Route::get('/HChef_division', [App\Http\Controllers\ControllerDivision::class, 'home']);
+Route::get('/ListeDemandesN', [App\Http\Controllers\ControllerDivision::class, 'get_list_ask_n_cd']);
+Route::get('/Formulaire/{id_user}', [App\Http\Controllers\ControllerDivision::class, 'get_form_ask'])->name('get_form_ask_cd');
+Route::post('/FormulaireSave{id_user}', [App\Http\Controllers\ControllerDivision::class, 'store_form_ask'])->name('store_form_ask_cd');
+Route::get('/ListeDemandes', [App\Http\Controllers\ControllerDivision::class, 'get_list_ask']);
+Route::get('/ListeDetaille{id_ask}', [App\Http\Controllers\ControllerDivision::class, 'get_list_details_ask'])->name('get_list_details_ask_cd');
+Route::get('/RechecherDemande{id_user}',  [App\Http\Controllers\Controller::class, 'get_search_ask'])->name('get_search_ask_cd');
+Route::get('/MiseAJourDemande{id_user}', [App\Http\Controllers\ControllerDivision::class, 'get_update_form_ask'])->name('get_update_form_ask_cd');
+Route::post('/MiseAJourDemandeSave{id_user}', [App\Http\Controllers\ControllerDivision::class, 'store_update_form_ask'])->name('store_update_form_ask_cd');
+/*************************************** Fin route Chef division ***********************************/
+
+/**************************************** Debut route Chef bureau ********************************/
+/* Route::get('/HChef_bureau', [App\Http\Controllers\ControllerBureau::class, 'home']);
+Route::get('/liste_demand_n', [App\Http\Controllers\ControllerBureau::class, 'liste_demand_n']);
+Route::get('/chef_bureau/{id}', [App\Http\Controllers\ControllerBureau::class, 'formulair'])->name('voir_demand');
+Route::post('/Fchef_bureau{id}', [App\Http\Controllers\ControllerBureau::class, 'stor'])->name('store_formulaire_demand');
+Route::get('/liste_demand', [App\Http\Controllers\ControllerBureau::class, 'liste_demand']);
+Route::get('/searchb{id}',  [App\Http\Controllers\Controller::class, 'searchb'])->name('info.search.cb');
+Route::get('/detailles_demand/{id}', [App\Http\Controllers\ControllerBureau::class, 'detailles'])->name('detailles_demand');
+Route::get('/chef_bureaurMj{id}', [App\Http\Controllers\ControllerBureau::class, 'form'])->name('formulairecb_demande_mj');
+Route::post('/chef_bureaurS_Mj{id}', [App\Http\Controllers\ControllerBureau::class, 'store'])->name('cb_store');
+ */ /*************************************** Fin route Chef bureau ***********************************/
+
+
+
+/**************************************** Debut route Damf ********************************/
+/* Route::get('/HDamf', [App\Http\Controllers\ControllerDamf::class, 'home']);
+Route::get('/liste_dmd_n', [App\Http\Controllers\ControllerDamf::class, 'liste_dmd_n']);
+Route::get('/damf/{id}', [App\Http\Controllers\ControllerDamf::class, 'formulair'])->name('voir_dmd');
+Route::post('/Fdamf{id}', [App\Http\Controllers\ControllerDamf::class, 'stor'])->name('store_formulaire_dmd');
+Route::get('/liste_dmd', [App\Http\Controllers\ControllerDamf::class, 'liste_dmd']);
+Route::get('/detailles/{id}', [App\Http\Controllers\ControllerDamf::class, 'detailles'])->name('detailles_dmd');
+Route::get('/searchD{id}',  [App\Http\Controllers\Controller::class, 'searchdamf'])->name('info.search.d');
+Route::get('/damfMj{id}', [App\Http\Controllers\ControllerDamf::class, 'form'])->name('formulaireda_demande_mj');
+Route::post('/damfS_Mj{id}', [App\Http\Controllers\ControllerDamf::class, 'store'])->name('da_store');
+ */
+
+/*************************************** Fin route Damf ***********************************/
+
+
+/**************************************** Debut route Client ********************************/
+Route::get('/Client', [App\Http\Controllers\ControllerClient::class, 'home']);
+Route::post('/Fclient{id}', [App\Http\Controllers\ControllerClient::class, 'Fclient'])->name('Fclient');
+Route::get('/demande/pdf/{id}', [App\Http\Controllers\ControllerClient::class, 'createPDF']);
+
+/*************************************** Fin route Client ***********************************/
+
+
+/**Route Admin**********************************************************************/
+Route::get('/Admin', [App\Http\Controllers\ControllerAdminSGBD::class, 'home']);
+Route::get('/CheckUser{id}', [App\Http\Controllers\ControllerAdminSGBD::class, 'home'])->name('check_user');
+Route::get('/ModifyUser{id}', [App\Http\Controllers\ControllerAdminSGBD::class, 'modify_user'])->name('modify_user');
+Route::get('/Delete{id}', [App\Http\Controllers\ControllerAdminSGBD::class, 'delete_user'])->name('delete_user');
+Route::post('/Store{id}', [App\Http\Controllers\ControllerAdminSGBD::class, 'store_user'])->name('store_user');
+Route::get('/user/pdf', [App\Http\Controllers\ControllerAdminSGBD::class, 'createPDF']);
+
+/******************************************************************************* */
