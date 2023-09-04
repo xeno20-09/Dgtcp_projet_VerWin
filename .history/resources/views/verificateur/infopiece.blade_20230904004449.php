39d249bb3ee09edf7e106ebc5d388485da04d124
xeno20-09 <allegressecakpo93@gmail.com>
2023-09-04 02:21:02 +0200

    add-function

---
+++ .history/resources/views/verificateur/infopiece.blade_20230904004449.php
@@ -0,0 +1,145 @@
+@extends('layout.verificateur.header')
+@section('content')
+    <div class="container">
+        <h1 style="text-align: center;">
+            @foreach ($user as $item)
+                <a class="nav-link" href="#"> Mr/Mrs {{ $item->name }} <span
+                        class="badge rounded-pill badge-notification bg-danger"
+                        style="position: relative;bottom: 24px;right: 24px;">{{ $dmd_n_lu }}</span> </a>
+            @endforeach
+        </h1>
+        @foreach ($demande as $item_c)
+            <h1>Demande N° {{ $item_c->numero_doss }} </h1>
+
+            <form action="{{ route('store_form_piece_verificateur', $item_c->id) }}" method="post">
+                @csrf
+
+
+
+                @for ($i = 0; $i < $item_c->nombre_doc; $i++)
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="libellepiece">Libellé de la pièce</label>
+                                <input type="text"name="libellepiece[]" id="libellepiece"
+                                    placeholder="Libellé de la pièce" class="form-control" value="">
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="referencespiece">Références de la pièce
+                                </label>
+                                <input type="text" value="{{ $referencesPieces[$i] }}"name="referencespiece[]"
+                                    id="referencespiece" placeholder="Références de la pièce" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="date_piece">Date de la pièce</label>
+                                <input type="texte" value="{{ $date }}" name="date_piece[]" id="date_piece"
+                                    class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class='col'>
+
+                            <div class='form-group'>
+                                <?php if ($e == 'ok') {
+                                    echo "<label for='montantrestant'>Montant  Restant</label>";
+                                    echo "<input type='number' value='$restant' name='montantrestant[]' id='montantrestant'
+                                                                                                                                                                                                                            style='background-color: skyblue' placeholder='Montant de la Restant'
+                                                                                                                                                                                                                            value='$restant' class='form-control'>";
+                                } ?>
+                            </div>
+                        </div>
+                        @php
+                            $valeurMax = $restant;
+                        @endphp
+
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="montantligne">Montant de la ligne</label>
+                                <input type="number" name="montantligne[]" id="montantligne"
+                                    placeholder="Montant de la ligne" class="form-control">
+                            </div>
+                        </div>
+
+
+                        <div class="col">
+                            <div class="form-group">
+
+
+
+                                <?php if ($montantdmd <= $valeurMax || $valeurMax == null) {
+                                    echo "<label for='montantdmd'>Montant de la demande</label>";
+                                    echo "  <input type='number' value='$montantdmd' name=' id='montantdmd'
+                            placeholder='Montant de la demande' style='background-color: rgb(199, 255, 199)' class='form-control'>";
+                                }
+                                
+                                if ($valeurMax < $montantdmd && $valeurMax != null) {
+                                    echo "<label for='montantdmd'>Montant de la demande</label>";
+                                    echo "  <input type='number' value='$montantdmd' name=' id='montantdmd'
+                                                                                                                                                                                                                            placeholder='Montant de la demande' style='background-color: rgb(255, 112, 112)' class='form-control'>";
+                                }
+                                
+                                ?>
+a
+                            </div>
+                        </div>
+                    </div>
+                @endfor
+
+                <?php if ($montantdmd <= $valeurMax || $valeurMax == null) {
+                echo "<button type='submit' class='btn btn-primary'>Enregistrer</button>"
+            }
+            if ($valeurMax < $montantdmd && $valeurMax != null) {                echo "<button type='submit' class='btn btn-primary'>Enregistrer</button>"
+}
+
+                ?>
+            </form>
+        @endforeach
+    </div>
+@endsection
+{{--     @for ($i = 0; $i < $numberinput; $i++)
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="libellepiece{{ $i }}">Libellé de la pièce {{ $i + 1 }}</label>
+                                <input type="text" name="libellepiece[]" id="libellepiece{{ $i }}"
+                                    placeholder="Libellé de la pièce" class="form-control" value="{{ $file[$i] }}">
+                            </div>
+                        </div>
+                        @php
+                            $numdoss = $item_c->numero_doss;
+                            $ref = $filewithoutext[$i] . substr($numdoss, 0, 2);
+                        @endphp
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="referencespiece{{ $i }}">Références de la pièce
+                                    {{ $i + 1 }}</label>
+                                <input type="text" name="referencespiece[]" id="referencespiece{{ $i }}"
+                                    placeholder="Références de la pièce" class="form-control" value="{{ $ref }}">
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="date_piece{{ $i }}">Date de la pièce {{ $i + 1 }}</label>
+                                <input type="texte" value="{{ $date }}" name="date_piece[]"
+                                    id="date_piece{{ $i }}" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="montantligne{{ $i }}">Montant de la ligne
+                                    {{ $i + 1 }}</label>
+                                <input type="number" name="montantligne[]" id="montantligne{{ $i }}"
+                                    placeholder="Montant de la ligne" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                @endfor --}}
