e4fd1a56a96a429548531fc9b73468a5f28647e8
xeno20 <allegressecakpo93@gmail.com>
2023-09-20 11:51:05 +0200

    verificateur

---
+++ .history/resources/views/verificateur/formv_demande_mj.blade_20230920113046.php
@@ -0,0 +1,354 @@
+@extends('layout.verificateur.header')
+@section('content')
+    <div class="container">
+        <h1 style="text-align: center;">
+            @foreach ($user as $item)
+                <a class="nav-link" href="#"> Mr/Mrs {{ $item->name }}  <span class="badge rounded-pill badge-notification bg-danger"
+                    style="position: relative;bottom: 24px;right: 24px;">{{ $dmd_n_lu }}</span>  </a>
+            @endforeach
+        </h1>
+        @foreach ($demande as $item_c)
+            <h1>Demande N° {{ $item_c->numero_doss }} </h1>
+
+
+            <form action="{{ url('VerificateurS_Mj', $item_c->id) }}" method="Post" class="card-body cardbody-color p-lg-5"
+                enctype="multipart/form-data">
+
+                @csrf
+
+
+                <div class="row">
+                    <legend>Checker l'enregistrement</legend>
+                    <hr>
+
+
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="" class="form-label mt-4">Date de dépôt du dossier</label>
+                            <input value="{{ $item_c->date }}" name="date_depot" type="date" class="form-control"
+                                id="" aria-describedby="" placeholder="">
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="" class="form-label mt-4">Nature des opérations</label>
+
+                            <input value="{{ $item_c->nature_op }}" name="nature_op" type="text" class="form-control"
+                                id="" placeholder="Nature des opérations">
+
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="" class="form-label mt-4">Nature des produits</label>
+
+                            <input value="{{ $item_c->nature_p }}" name="nature_pro" type="text" class="form-control"
+                                id="" placeholder="Nature des produits">
+
+                        </div>
+                    </div>
+
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="" class="form-label mt-4">Devise </label>
+                            <input value="{{ $item_c->devise }}" name="currency_from" type="text" class="form-control"
+                                id="" placeholder="">
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="" class="form-label mt-4">Montant </label>
+                            <input value="{{ $item_c->montant }}" name="montant_in" type="number" class="form-control"
+                                id="" placeholder="Montant en Francs">
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="" class="form-label mt-4">Montant en Francs</label>
+                            <input value="{{ $item_c->montant_con }}" name="montant_out" type="number" class="form-control"
+                                id="" placeholder="Montant converti">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="" class="form-label mt-4">Nom client</label>
+                            <input value="{{ $item_c->nom_client }}" name="nom_client" type="text" class="form-control"
+                                id="" placeholder="Nom client">
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="" class="form-label mt-4">Prenom client</label>
+                            <input value="{{ $item_c->prenom_client }}" name="prenom_client" type="text"
+                                class="form-control" id="" placeholder="Prenom client">
+                        </div>
+                    </div>
+
+
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="" class="form-label mt-4">Telephone client</label>
+                            <input value="{{ $item_c->tel_client }}" name="tel_client" type="text" class="form-control"
+                                id="" placeholder="TelClient">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+
+
+
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="" class="form-label mt-4">Banque</label>
+                            <input value="{{ $item_c->banque_client }}" name="banque_client" type="text"
+                                class="form-control" id="" placeholder="Banque">
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="" class="form-label mt-4">Profession client</label>
+                            <input value="{{ $item_c->profess_client }}" name="profess_client" type="text"
+                                class="form-control" id="" placeholder="ProfClient">
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="" class="form-label mt-4">Numéro de compte</label>
+                            <input value="{{ $item_c->num_compt_client }}" name="num_compt_client" type="text"
+                                class="form-control" id="" placeholder="Numéro de compte">
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <div class="row">
+
+
+
+                    <div class="col">
+                        <div class="form-group">
+
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="form-group">
+
+                        </div>
+                    </div>
+
+                </div>
+                <br><br>
+                <legend>Saisir d'une demande</legend>
+                <hr>
+
+                <div class="row">
+
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="" class="form-label mt-4">Nom du beneficiaire</label>
+                            <input name="nom_benifi" type="text" class="form-control" id=""
+                                placeholder="Nom du beneficiaire" value="{{ $item_c->nom_benefi }}">
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="" class="form-label mt-4">Prenom du beneficiaire</label>
+                            <input name="prenom_benifi" type="text" class="form-control" id=""
+                                placeholder="Prenom du beneficiaire" value="{{ $item_c->prenom_benefi }}">
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="" class="form-label mt-4">Profession du beneficiaire</label>
+                            <input name="profess_benifi" type="text" class="form-control" id=""
+                                placeholder="Profession du beneficiaire" value="{{ $item_c->profess_benefi }}">
+                        </div>
+                    </div>
+
+                </div>
+
+
+                <div class="row">
+
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="" class="form-label mt-4">Pays benificiaire</label>
+                            <input name="pays_benifi" type="text" class="form-control" id=""
+                                placeholder="PaysClient" value="{{ $item_c->pays_benifi }}">
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="" class="form-label mt-4">Banque beneficiaire</label>
+                            <input name="banque_benifi" type="text" class="form-control" id=""
+                                placeholder="Banque" value="{{ $item_c->banque_benefi }}">
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="" class="form-label mt-4">Numéro de compte beneficiaire</label>
+
+                            <input maxlength="12" minlength="12" name="num_compt_benifi" type="text"
+                                class="form-control" id="" placeholder="Numéro de compte"
+                                value="{{ $item_c->num_compt_benefi }}">
+
+                        </div>
+                    </div>
+
+
+                </div>
+
+              {{--   <div class="row">
+
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="file">Pieces joint</label>
+                            <input type="file" class="form-control-file" name="file[]" multiple>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="form-group">
+
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="form-group">
+
+                        </div>
+                    </div>
+                </div> --}}
+                @foreach ($piece as $item_c)
+
+                <div id="container" style="  width: 1362px;">
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="" class="form-label mt-4">Pieces</label>
+                                <input name="pieces_doss[]" type="text" value="{{ $item_c->libellepiece }}" class="form-control" id="pieces" placeholder="pieces">
+                            </div>
+                        </div>
+                        
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="" class="form-label mt-4">Reference piece</label>
+                                <input name="ref_doss[]" type="text" class="form-control"value="{{ $item_c->referencespiece }}" id="refs" placeholder="ref pieces">
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="" class="form-label mt-4">Montant ligne</label>
+                                <input name="montantligne[]" type="number" class="form-control" value="{{ $item_c->montantligne }}" id="mligne" placeholder="Montant ligne">
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="" class="form-label mt-4">Date d'expiration de la piece</label>
+                                <input name="exp_pieces[]" value="{{ $item_c->dateexpi }}"  type="date" class="form-control" id="expi" placeholder="date expiration pieces">
+                            </div>
+                        </div>
+                    {{-- </div>
+                    <div class="row"> --}}
+                      
+                        
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="" class="form-label mt-4">Ajouter une autre piece</label>
+                                <button  style="position: relative; top: 36px; right: 100px;" type="button" class="btn btn-primary" name="btn1" id="ajouterChamp">+</button>
+                            </div>
+                        </div>
+                        <div class="col" id="retirer">
+                            <div class="form-group">
+                                <label for="" class="form-label mt-4" id="lretirerChamp">Retirer la piece</label>
+                                <button  style="position: relative; top: 36px; right: 100px;" type="button" class="btn btn-danger" name="btn2" id="retirerChamp">-</button>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+                @endforeach
+                <br>
+
+
+                <button type="submit" class="btn btn-primary">Enregistrer</button>
+            </form>
+            <a href="{{ route('retour_s', $item_c->id) }}"><button class="btn btn-danger">Retournez la
+                    demande au Secretaire </button></a>
+        @endforeach
+
+
+    </div>
+
+
+    <br>
+    <br>
+@endsection
+
+<script>
+    // Définissez une variable globale pour stocker la valeur de i
+    var i = 0;
+
+    document.addEventListener("DOMContentLoaded", function () {
+        var boutonRetirer = document.getElementById("retirerChamp");
+
+        var boutonAjouter = document.getElementById("ajouterChamp");
+
+        // Sélectionnez le conteneur des rangées
+        var container = document.getElementById("container");
+      
+        // Ajoutez un gestionnaire d'événement au bouton "Ajouter"
+        boutonAjouter.addEventListener("click", function () {
+            
+            // Incrémente le compteur de clics
+
+            // Clonez la première rangée
+            
+            var clonedRow = container.querySelector(".row").cloneNode(true);
+            
+            // Effacez les valeurs des champs clonés
+            var inputs = clonedRow.querySelectorAll("input");
+            inputs.forEach(function (input) {
+                input.value = "";
+            });
+
+            // Changez les noms des champs et réinitialisez les valeurs
+            var piece = clonedRow.querySelector("#pieces");
+            var refs = clonedRow.querySelector("#refs");
+            var datex = clonedRow.querySelector("#expi");
+            var mligne = clonedRow.querySelector("#mligne");
+            var btn1 = clonedRow.querySelector("#retirerChamp");
+            var btn2 = clonedRow.querySelector("#ajouterChamp");
+
+            piece.value = "";
+            refs.value = "";
+            datex.value = "";
+            mligne.value = " ";
+            piece.name = "pieces_doss[]";
+            refs.name = "ref_doss[]";
+            datex.name = "exp_pieces[]";
+            mligne.name = "montantligne[]";
+btn1.name='btn1';
+btn2.name='btn2';
+
+
+                container.appendChild(clonedRow);
+       
+        });
+        boutonRetirer.addEventListener("click", function () {
+            var rows = container.querySelectorAll(".row");
+                var lastRow = rows[rows.length - 1];
+            container.removeChild(lastRow).remove();
+
+        });
+    });
+</script>
---
+++ .history/resources/views/verificateur/infopiece.blade_20230920103004.php
@@ -0,0 +1,162 @@
+@extends('layout.verificateur.header')
+
+@section('content')
+    <div class="container">
+        <h1 style="text-align: center;">
+            @foreach ($user as $item)
+                <a class="nav-link" href="#"> Mr/Mrs {{ $item->name }} <span
+                        class="badge rounded-pill badge-notification bg-danger"
+                        style="position: relative;bottom: 24px;right: 24px;">{{ $dmd_n_lu }}</span> </a>
+            @endforeach
+        </h1>
+
+        @foreach ($demande as $item_c)
+            <h1>Demande N° {{ $item_c->numero_doss }} </h1>
+
+            <form action="{{ route('store_form_piece_verificateur', $item_c->id) }}" method="post">
+                @csrf
+                @php
+                $valeurMax = $restant;
+
+              
+            @endphp
+                @for ($i = 0; $i < $item_c->nombre_doc; $i++)
+                <div>
+                    <hr>                </div>
+               
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="libellepiece">Libellé de la pièce</label>
+                                <input type="text" name="libellepiece[]" id="libellepiece"
+                                    placeholder="Libellé de la pièce" class="form-control"
+                                    value="{{ $libellePiece[$i] }}"readonly>
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="referencespiece">Références de la pièce</label>
+                                <input type="text" value="{{ $referencesPieces[$i] }}" name="referencespiece[]"
+                                    id="referencespiece" placeholder="Références de la pièce" class="form-control"readonly>
+                            </div>
+                        </div>
+
+                        @if ($datePiece[$i] != null)
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="datepiece{{ $i }}">Date d'expiration</label>
+                                    <input type="texte" value="{{ $datePiece[$i] }}" name="date_expi[]"
+                                        id="datepieceex" placeholder="Date d'expiration" class="form-control"readonly>
+                                </div>
+                            </div>
+                        @endif
+
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="date_piece">Date de la pièce</label>
+                                <input type="texte" value="{{ $date }}" name="date_piece[]" id="date_piece"
+                                    class="form-control"readonly>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                   
+                        @if ($montantligne[$i] == null)
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="montantligne">Montant de la ligne</label>
+                                <input type="number" name="montantligne[]" id="montantligne"
+                                    placeholder="Montant de la ligne" value="{{$montantligne[$i]}}" class="form-control">
+                            </div>
+                        </div>
+                        @elseif ($montantligne[$i] != null)
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="montantligne">Montant de la ligne</label>
+                                <input type="number" name="montantligne[]" id="montantligne"
+                                    placeholder="Montant de la ligne"  value="{{$montantligne[$i]}}" class="form-control"readonly>
+                            </div>
+                        </div>
+                        @endif
+                        <div class="col">
+                            <div class="form-group">
+                                @if (($restant!=0)&&( $restant>$montantligne[$i])&&($restant!='r'))
+                                    <label for='montantdmd'>Montant de la demande</label>
+                                    <input type='number' value='{{ $montantdmd }}' name='montantdmd[]'
+                                        id='montantdmd' placeholder='Montant de la demande'
+                                        class='form-control'readonly>
+                                        @endif           
+                                        @if ( ($restant==0)&&($restant!='r'))
+                                        <label for='montantdmd'>Montant de la demande</label>
+                                        <input type='number' value='{{ $montantdmd }}' name='montantdmd[]'
+                                            id='montantdmd' style='background-color: red' placeholder='Montant de la demande'
+                                            class='form-control'readonly>
+                                            
+                                @endif
+                                @if ( (($restant<$montantligne[$i])||($montantdmd>$montantligne[$i]))&&($restant!='r'))
+                                <label for='montantdmd'>Montant de la demande</label>
+                                <input type='number' value='{{ $montantdmd }}' name='montantdmd[]'
+                                    id='montantdmd' style='background-color: red' placeholder='Montant de la demande'
+                                    class='form-control'readonly>
+    
+                                @endif
+                                @php
+                                    if( (($restant<$montantligne[$i])||($montantdmd>$montantligne[$i]))&&($restant!='r')) {
+                                        <label for='montantdmd'>Montant de la demande</label>;
+                                <input type='number' value='{{ $montantdmd }}' name='montantdmd[]'
+                                    id='montantdmd' style='background-color: red' placeholder='Montant de la demande'
+                                    class='form-control'readonly>;
+                                    }
+                                @endphp
+                                @if ( ($montantdmd<$montantligne[$i])&&($restant=='r'))
+                                <label for='montantdmd'>Montant de la demande</label>
+                                <input type='number' value='{{ $montantdmd }}' name='montantdmd[]'
+                                    id='montantdmd' style='background-color:skyblue' placeholder='Montant de la demande'
+                                    class='form-control'readonly>
+    
+                                @endif
+                            </div>
+                        </div>
+                             <div class='col'>
+                            <div class='form-group'>
+                                @if ($e == 'ok')
+                                    <label for='montantrestant'>Montant  Restant</label>
+                                    <input type='number' value='{{ $restant }}' name='montantrestant[]'
+                                        id='montantrestant' style='background-color: skyblue'
+                                        placeholder='Montant de la Restant' class='form-control'readonly>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                  
+                @endfor
+
+      
+                 
+                @for ($i = 0; $i < $item_c->nombre_doc; $i++)
+
+                    @if ( $restant>$montantligne[$i] ) 
+                    <div class="row mt-5">
+                    <div class="col">
+                        <div class="form-group">
+                    <button type='submit' class='btn btn-primary'>Enregistrer</button>
+                </div>
+            </div>
+             @endif
+             @endfor
+             </form> 
+{{--             @if (  $montantdmd>$valeurMax )
+ --}}            <div class="col">
+                <div class="form-group">
+                <a href='{{ route('rejet_piece_verificateur', $item_c->id) }}'>
+                    <button class='btn btn-danger'>Rejeter</button>
+                </a>
+            </div>
+        </div>
+    </div>
+{{--         @endif
+ --}}        @endforeach
+    </div>
+@endsection
